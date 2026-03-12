858f42e9a195ff236d7cb6cc4cf7aa8f75823af2
roshirif <152089684+roshirif@users.noreply.github.com>
2026-03-13 02:42:38 +0900

    Update flip.js

--- js/flip.js
+++ js/flip.js
@@ -1,6 +1,6 @@
 function handleTickInit(tick) {
   // カウントダウンの終了日時
-  const targetDate = "2025-12-14T17:00:00+09:00";
+  const targetDate = "2024-12-14T17:00:00+09:00";
 
   // カウントダウンを作成
   const counter = Tick.count.down(targetDate);
@@ -9,8 +9,9 @@ function handleTickInit(tick) {
     tick.value = value;
   };
 
+  // カウントが終了したときの処理（何もしないように空っぽにする）
   counter.onended = function() {
-    // 今の画面を消さずに、bodyの一番最後に文字を追加する
-    document.body.insertAdjacentHTML('beforeend', '<h1>🎶 演奏発表会 開演！</h1>');
+    // ここに何も書かないことで、画面はそのままになります
   };
 }
+
